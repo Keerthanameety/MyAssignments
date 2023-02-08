c2af4dd2fc30c9c5f74b595fb097c7b04afeb6dc
Keerthanameety <124448420+Keerthanameety@users.noreply.github.com>
2023-02-08 22:11:37 +0530

    Add files via upload

---
+++ day2/negativeToPositive.java
@@ -0,0 +1,13 @@
+package week1.day2;
+
+public class negativeToPositive {
+
+	public static void main(String[] args) {
+		int negativeNumber = -40;
+		int positiveNumber;
+                if (negativeNumber < 0) {
+            positiveNumber = negativeNumber * (-1);
+            System.out.println("The positive Number is " +positiveNumber);
+	}
+	}
+	}
